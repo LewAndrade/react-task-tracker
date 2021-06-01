ca9b62e28ee95f540c5a754d897c1837ce8c1492
Lew Andrade <andrade.lew@gmail.com>
2021-06-01 05:48:30 -0300

    Create Add Task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -30,6 +31,12 @@ function App() {
     },
   ]);
 
+  const addTask = (task) => {
+    const id = tasks[tasks.length - 1].id + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  };
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
@@ -45,6 +52,7 @@ function App() {
   return (
     <div className={"container"}>
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleTask} />
       ) : (
